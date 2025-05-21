08ee946511077df26d77dfb5ca58bf75c9a4e5b2
itstijnam <manjitsyngh@gmail.com>
2025-05-21 18:25:09 +0530

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import './Header.scss'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch }  from 'react-redux'
+import LOGO from '../../assets/arch_space.png'
 
 function Header() {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const archNav = [
+    {
+      text: 'Home',
+      url: '',
+    },
+    {
+      text: 'Service',
+      url: '',
+    },
+    {
+      text: 'About',
+      url: '',
+    },
+    {
+      text: 'Portfolio',
+      url: '',
+    },
+  ]
+
+  const getCapName = (text)=>{
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
+
+  const navHandler = (text)=>{
+    if(text === 'Home'){
+      navigate('/')
+    } else if( text === 'Service'){
+      navigate('/')
+      
+    }else if( text === 'About'){
+      navigate('/')
+      
+    }else if( text === 'Portfolio'){
+      navigate('/')
+      
+    }
+  }
+
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className="arch_logo">
+          <img src={LOGO} alt="" />
+        </div>
+        <div className="arch_header_nav">
+            <ul className='arch_nav_ul'>
+              {archNav.map((arch, i)=>(
+                <li key={i} onClick={()=>navHandler(arch.text)} >{getCapName(arch.text)}</li>
+              ))}
+            </ul>
+            <button className='arch_contact_btn'>Contact Us</button>
+        </div>
+    </div>
   )
 }
 
