41f414f7cd4c924ee40aaf60222f3995ba2c4a40
manjitsyngh <manjitsingh13septem@gmail.com>
2025-06-06 01:26:24 +0530

    url

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,4 +1,5 @@
 
 
 // export const baseUrl = `http://localhost:5000`;
-export const baseUrl = `https://archspace.onrender.com`;
+// export const baseUrl = `https://archspace.onrender.com`;
+export const baseUrl = `https://basic-hn7q.vercel.app`;
